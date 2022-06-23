3929d2aaa5e652243bc2f29b1413f3646ec0cad4
caezaocta <za2prayuda@gmail.com>
2022-06-23 19:25:44 +0700

    remove navbar from landing

--- src/pages/Landing/index.js
+++ src/pages/Landing/index.js
@@ -1,16 +1,14 @@
-import Button from '../../components/atoms/Button'
-import ButtonAuth from '../../components/atoms/ButtonAuth'
-import Navbar from '../../components/organisms/Navbar'
+import Button from '../../components/atoms/Button';
+import ButtonAuth from '../../components/atoms/ButtonAuth';
 
 const Landing = () => {
     return (
         <>
-            <Navbar></Navbar>
             <Button header="Search Now"></Button>
             <br />
             <ButtonAuth header="Log In"></ButtonAuth>
         </>
-    )
-}
+    );
+};
 
 export default Landing;
