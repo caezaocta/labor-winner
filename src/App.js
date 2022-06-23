6d4eab6a00e905f72c8ccf4e21f6aaf86bce870b
caezaocta <za2prayuda@gmail.com>
2022-06-23 21:14:02 +0700

    navbar and custom mui theme

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import Landing from '../src/pages/Landing'
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#16609D',
+    },
+    secondary: {
+      main: '#D11614'
+    },
+  },
+  button: {
+    typography: {
+      textTransform: 'none'
+    }
+  }
+})
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <Landing />
+      </div>
+    </ThemeProvider>
   );
 }
 
